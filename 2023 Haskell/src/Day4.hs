b5f5ac49867ccdfb926957ca82f5e9a53ec29d57
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-12 21:30:21 +0000

    refactor: do not use unnecessary dolar

--- 2023 Haskell/src/Day4.hs
+++ 2023 Haskell/src/Day4.hs
@@ -14,7 +14,7 @@ cardPoints card =
       barIndex = unwrap $ '|' `elemIndex` restOfCard
       (winningNumbers, currentNumbers) = join (***) words (barIndex `splitAt` restOfCard)
       myWinningNumbers = length $ filter (`elem` winningNumbers) (drop 1 currentNumbers)
-   in traceShowId $ (if myWinningNumbers == 0 then 0 else (2 ^ (myWinningNumbers - 1)))
+   in traceShowId (if myWinningNumbers == 0 then 0 else 2 ^ (myWinningNumbers - 1))
 
 part1 :: String -> String
 part1 input = show $ sum $ map cardPoints $ lines input
