754ddd64faf471f9b42e8eaa4fe252cf770c2add
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-12 16:21:55 +0000

    refactor: extract getSurroundings into two functions

--- 2023 Haskell/src/Day3.hs
+++ 2023 Haskell/src/Day3.hs
@@ -2,6 +2,7 @@ module Day3 where
 
 import Data.Char (isDigit)
 import Data.Maybe (catMaybes)
+-- import Debug.Trace (traceShowId)
 import Lib (enumerate, maybeAt, slice)
 
 -- | Whether a character is considered a symbol
@@ -46,7 +47,26 @@ numberParser acc (i, ch)
 parseLine :: String -> [NumberRange]
 parseLine = foldl numberParser [] . enumerate
 
+-- | (line number, index)
+type CharPosition = (Int, Int)
+
 -- | Obtains adjacent chars around a slice in a list of strings
+--
+-- # Examples
+--
+-- >>> getAdjacentChars [ "A-----B", "@hello|", "C+++++D" ] $ getAdjacentCharPositions [ "A-----B", "@hello|", "C+++++D" ] (1, 1, 5)
+--   "A-----B",
+--   "@hello|",
+--   "C+++++D"
+-- ] (1, 1, 5)
+-- "ABCD-----+++++|@"
+--
+-- >>> getAdjacentChars [ "A-----B", "@hello|", "C+++++D" ] $ getAdjacentCharPositions [ "A-----B", "@hello|", "C+++++D" ] (0, 1, 5)
+-- "@hello|AB"
+getAdjacentChars :: [String] -> [CharPosition] -> [Char]
+getAdjacentChars linesList = map (\(charLinenr, charIndex) -> (linesList !! charLinenr) !! charIndex)
+
+-- | Obtains positions of chars around a slice in a list of strings
 --
 -- # Examples
 --
@@ -55,31 +75,38 @@ parseLine = foldl numberParser [] . enumerate
 --   "@hello|",
 --   "C+++++D"
 -- ] (1, 1, 5)
--- "ABCD-----+++++|@"
---
--- >>> getAdjacentChars [
---   "A-----B",
---   "@hello|",
---   "C+++++D"
--- ] (0, 0, 3)
--- "l@hel-"
-getAdjacentChars :: [String] -> LinenrNumberRange -> [Char]
-getAdjacentChars linesList (linenr, from, to) =
-  let lineAbove = linesList `maybeAt` (linenr - 1)
+-- [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6),
+--  (1, 0),                                         (1, 6),
+--  (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6)]
+getAdjacentCharPositions :: [String] -> LinenrNumberRange -> [CharPosition]
+getAdjacentCharPositions linesList (linenr, from, to) =
+  let linenrAbove = linenr - 1
+      lineAbove = linesList `maybeAt` linenrAbove
       line = linesList `maybeAt` linenr
-      lineBelow = linesList `maybeAt` (linenr + 1)
-      atMaybe = flip maybeAt
-      prev = atMaybe $ from - 1
-      next = atMaybe $ to + 1
-      before = line >>= prev
-      after = line >>= next
-      topLeft = lineAbove >>= prev
-      topRight = lineAbove >>= next
-      bottomLeft = lineBelow >>= prev
-      bottomRight = lineBelow >>= next
+      linenrBelow = linenr + 1
+      lineBelow = linesList `maybeAt` linenrBelow
+
+      checkIndex charIndex lineNumber someLine
+        | charIndex < 0 || charIndex >= length someLine = Nothing
+        | otherwise = Just (lineNumber, charIndex)
+
+      before = line >>= checkIndex (from - 1) linenr
+      after = line >>= checkIndex (to + 1) linenr
+      topLeft = lineAbove >>= checkIndex (from - 1) linenrAbove
+      topRight = lineAbove >>= checkIndex (to + 1) linenrAbove
+      bottomLeft = lineBelow >>= checkIndex (from - 1) linenrBelow
+      bottomRight = lineBelow >>= checkIndex (to + 1) linenrBelow
+
+      -- leftRight = []
       leftRight = [before, after, topLeft, topRight, bottomLeft, bottomRight]
-      topRow = slice from (to + 1) <$> lineAbove
-      bottomRow = slice from (to + 1) <$> lineBelow
+
+      topSliced = (slice from (to + 1) <$> lineAbove)
+      bottomSliced = (slice from (to + 1) <$> lineBelow)
+      topZipped = flip zip [from ..] <$> topSliced
+      bottomZipped = flip zip [from ..] <$> bottomSliced
+
+      topRow = (((linenrAbove,) . snd) <$>) <$> topZipped
+      bottomRow = (((linenrBelow,) . snd) <$>) <$> bottomZipped
    in concat
         ( catMaybes
             [ topRow,
@@ -106,7 +133,7 @@ getAdjacentChars linesList (linenr, from, to) =
 -- ] (1, 1, 4)
 -- False
 isPartNumber :: [String] -> LinenrNumberRange -> Bool
-isPartNumber linesList numberRange = any isSymbol $ getAdjacentChars linesList numberRange
+isPartNumber linesList numberRange = any isSymbol $ getAdjacentChars linesList $ getAdjacentCharPositions linesList numberRange
 
 -- | Adds line number information to each number position and flattens
 --
@@ -127,9 +154,8 @@ parsePartNumbers allNumbers allLines = map parsePartNumber allNumbers
   where
     parsePartNumber (linenr, from, to) = read (slice from (to + 1) (allLines !! linenr))
 
+getAllPartNumbers :: [String] -> [LinenrNumberRange]
+getAllPartNumbers allLines = filter (isPartNumber allLines) $ addLineInfo $ map parseLine allLines
+
 part1 :: String -> String
-part1 input =
-  let parsedNumbers = addLineInfo $ map parseLine allLines
-      partNumbers = filter (isPartNumber allLines) parsedNumbers
-      allLines = lines input
-   in show $ sum $ parsePartNumbers partNumbers allLines
+part1 input = show $ sum $ parsePartNumbers (getAllPartNumbers $ lines input) $ lines input
