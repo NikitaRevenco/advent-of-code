1a6b4b94661337bfd5b0b09fbd844c23dd7e2a73
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-13 20:09:13 +0000

    refactor: 2023 day 7

--- 2023 Haskell/src/Day4.hs
+++ 2023 Haskell/src/Day4.hs
@@ -3,7 +3,6 @@ module Day4 where
 import Control.Arrow (Arrow ((***)))
 import Control.Monad (join)
 import Data.List (elemIndex)
-import Debug.Trace (traceShowId)
 import Lib (unwrap)
 
 cardPoints :: String -> Int
@@ -14,7 +13,7 @@ cardPoints card =
       barIndex = unwrap $ '|' `elemIndex` restOfCard
       (winningNumbers, currentNumbers) = join (***) words (barIndex `splitAt` restOfCard)
       myWinningNumbers = length $ filter (`elem` winningNumbers) (drop 1 currentNumbers)
-   in traceShowId (if myWinningNumbers == 0 then 0 else 2 ^ (myWinningNumbers - 1))
+   in if myWinningNumbers == 0 then 0 else 2 ^ (myWinningNumbers - 1)
 
 part1 :: String -> String
 part1 input = show $ sum $ map cardPoints $ lines input
--- 2023 Haskell/src/Day7.hs
+++ 2023 Haskell/src/Day7.hs
@@ -7,23 +7,30 @@ import Lib (countOccurrences, unwrap)
 
 data HandType = HighCard | OnePair | TwoPair | ThreeOfKind | FullHouse | FourOfKind | FiveOfKind deriving (Enum, Eq, Ord, Show)
 
-data Card = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Enum, Eq, Ord, Show)
+data Card = Joker | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Enum, Eq, Ord, Show)
 
-charToCard :: Char -> Card
-charToCard '2' = Two
-charToCard '3' = Three
-charToCard '4' = Four
-charToCard '5' = Five
-charToCard '6' = Six
-charToCard '7' = Seven
-charToCard '8' = Eight
-charToCard '9' = Nine
-charToCard 'T' = Ten
-charToCard 'J' = Jack
-charToCard 'Q' = Queen
-charToCard 'K' = King
-charToCard 'A' = Ace
-charToCard _ = error "Could not parse character as card"
+charToCardImpl :: Char -> Card
+charToCardImpl '2' = Two
+charToCardImpl '3' = Three
+charToCardImpl '4' = Four
+charToCardImpl '5' = Five
+charToCardImpl '6' = Six
+charToCardImpl '7' = Seven
+charToCardImpl '8' = Eight
+charToCardImpl '9' = Nine
+charToCardImpl 'T' = Ten
+charToCardImpl 'Q' = Queen
+charToCardImpl 'K' = King
+charToCardImpl 'A' = Ace
+charToCardImpl _ = error "Could not parse character as card"
+
+charToCardJoker :: Char -> Card
+charToCardJoker 'J' = Joker
+charToCardJoker ch = charToCardImpl ch
+
+charToCardJack :: Char -> Card
+charToCardJack 'J' = Jack
+charToCardJack ch = charToCardImpl ch
 
 data Hand = Hand
   { handType :: HandType,
@@ -32,50 +39,8 @@ data Hand = Hand
   }
   deriving (Eq)
 
-getHandType :: [Card] -> HandType
+getHandType :: (Ord a) => [a] -> HandType
 getHandType cards
-  -- All 5 cards have the same label
-  | length occurences == 1 = FiveOfKind
-  -- 4 cards share a label and 1 card has a different label
-  | length occurences == 2 && (4 `elem` counts && 1 `elem` counts) =
-      FourOfKind
-  -- 3 cards share a label and 2 cards have a different label
-  | length occurences == 2 && (2 `elem` counts && 3 `elem` counts) =
-      FullHouse
-  -- 3 cards share a label, 2 cards each have unique labels
-  | length occurences == 3 && (3 `elem` counts) =
-      ThreeOfKind
-  -- 2 cards share a label, 2 cards share a different label, 1 card has a different label
-  | length occurences == 3 && (1 `elem` counts && 2 `elem` counts) =
-      TwoPair
-  -- 2 cards share a label, 3 cards each have unique labels
-  | length occurences == 4 && 2 `elem` counts =
-      OnePair
-  -- All 5 of the cards' labels are distinct
-  | length occurences == 5 = HighCard
-  | otherwise = error "Card does not match any pattern"
-  where
-    occurences = Map.toList $ countOccurrences cards
-    counts = map snd occurences
-
-parseLine :: String -> (Hand, Int)
-parseLine line =
-  let both = words line
-      bid' = read $ both !! 1
-      cards = map charToCard $ head both
-      cardType = getHandType cards
-      hand = Hand cardType cards
-   in (hand, bid')
-
-part1 :: String -> String
-part1 input =
-  let parsed = map parseLine $ lines input
-      sorted = map snd $ sort parsed
-      totalWinnings = sum $ zipWith (*) sorted [1 ..]
-   in show totalWinnings
-
-getHandTypeJ :: [JCard] -> HandType
-getHandTypeJ cards
   -- All 5 cards have the same label
   | length occurences == 1 = FiveOfKind
   -- 4 cards share a label and 1 card has a different label
@@ -103,57 +68,52 @@ getHandTypeJ cards
 data JHand = JHand
   { jhandType :: HandType,
     -- has 5 cards always
-    jcards :: [JCard]
+    jcards :: [Card]
   }
   deriving (Eq, Ord)
 
 instance Ord Hand where
-  compare hand1@(Hand handType1 cards1) hand2@(Hand handType2 cards2)
+  compare (Hand handType1 cards1) (Hand handType2 cards2)
     | handType1 == handType2 = compare cards1 cards2
     | otherwise = compare handType1 handType2
 
-data JCard = Joker | JTwo | JThree | JFour | JFive | JSix | JSeven | JEight | JNine | JTen | JQueen | JKing | JAce deriving (Enum, Eq, Ord, Show)
-
-charToJCard :: Char -> JCard
-charToJCard '2' = JTwo
-charToJCard '3' = JThree
-charToJCard '4' = JFour
-charToJCard '5' = JFive
-charToJCard '6' = JSix
-charToJCard '7' = JSeven
-charToJCard '8' = JEight
-charToJCard '9' = JNine
-charToJCard 'T' = JTen
-charToJCard 'J' = Joker
-charToJCard 'Q' = JQueen
-charToJCard 'K' = JKing
-charToJCard 'A' = JAce
-charToJCard _ = error "Could not parse character as card"
-
-resolveJokers :: [JCard] -> [[JCard]]
-resolveJokers = pure
-
-jokerMorphs = [JTwo .. JAce]
-
-findJCard :: [(Int, JCard)] -> Int -> JCard
+findJCard :: [(Int, Card)] -> Int -> Card
 findJCard xs num = snd $ unwrap $ find ((== num) . fst) xs
 
-parseLineJ :: String -> (JHand, Int)
-parseLineJ line =
-  let both = words line
-      bid' = read $ both !! 1
-      originalCards = map charToJCard $ head both
+-- | splits into the unparsed Card and the bid for that card
+splitLine :: String -> (String, Int)
+splitLine line = (head parts, read $ parts !! 1)
+  where
+    parts = words line
+
+parseLineJack :: String -> (Hand, Int)
+parseLineJack line =
+  let (unparsedCard, bid) = splitLine line
+      cards = map charToCardJack unparsedCard
+      cardType = getHandType cards
+      hand = Hand cardType cards
+   in (hand, bid)
+
+possibleJokerTransformations :: [Card]
+possibleJokerTransformations = [Two .. Ten] ++ [Queen .. Ace]
+
+parseLineJoker :: String -> (JHand, Int)
+parseLineJoker line =
+  let (unparsedCard, bid) = splitLine line
+      originalCards = map charToCardJoker unparsedCard
       -- list of positions where Joker appears
       jokerAppears = elemIndices Joker originalCards
       -- all the possible permutations of the N amount of times that we see Joker card
-      possibleJokerMorphs = map (zip jokerAppears) $ replicateM (length jokerAppears) jokerMorphs
+      possibleJokerMorphs = map (zip jokerAppears) $ replicateM (length jokerAppears) possibleJokerTransformations
+
       possibleCards =
         map
           ( \jokerTransformation ->
               zipWith
                 ( \originalCardIndex originalCard ->
                     ( if originalCard == Joker
-                        then findJCard jokerTransformation originalCardIndex
+                        then -- Joker transforms into a different card
+                          findJCard jokerTransformation originalCardIndex
                         else originalCard
                     )
                 )
@@ -161,13 +121,18 @@ parseLineJ line =
                 originalCards
           )
           possibleJokerMorphs
-      bestCard = maximum (map getHandTypeJ possibleCards)
+      bestCard = maximum (map getHandType possibleCards)
       hand = JHand bestCard originalCards
-   in (hand, bid')
+   in (hand, bid)
 
-part2 :: String -> String
-part2 input =
-  let parsed = map parseLineJ $ lines input
+solve parseLine input =
+  let parsed = map parseLine $ lines input
       sorted = map snd $ sort parsed
       totalWinnings = sum $ zipWith (*) sorted [1 ..]
    in show totalWinnings
+
+part1 :: String -> String
+part1 = solve parseLineJack
+
+part2 :: String -> String
+part2 = solve parseLineJoker
